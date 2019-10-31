0a4fc62b8615a59af246f17ec9155f50e6447446
Samwell1996 <kostinvasil5@gmail.com>
2019-11-01 00:33:11 +0200

    add button back *

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './HomePage.css'
-import {ReviewsDetails} from '../index'
 
 export const HomePage = () => {
 
     return (
         <div className='HomePage'>
             Welcome to Home
-            <ReviewsDetails/>
         </div>
     )
 };
